a46e2eb52fc2ca6cf5a80b123e787b0ecf78ba3a
joaopombos <pv20407@alunos.estgv.ipv.pt>
2024-07-17 16:39:30 +0100

    link list

--- src/views/ListAdmin.js
+++ src/views/ListAdmin.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../CSS/ware.css';
 
@@ -82,23 +82,23 @@ const ListAdmin = () => {
         </div>
         <ul class="components">
           <li>
-            <a href="/add/admin"><i class="fas fa-plus"></i> Adicionar Software/Addon</a>
+            <Link to="/add/admin"><i class="fas fa-plus"></i> Adicionar Software/Addon</Link>
           </li>
           <li class="active">
-            <a href="/list/admin"><i class="fas fa-list"></i> Listar Softwares/Addons</a>
+            <Link to="/list/admin"><i class="fas fa-list"></i> Listar Softwares/Addons</Link>
           </li>
           <li>
-            <a href="/list/admin/clientes"><i class="fas fa-list"></i> Listar Clientes</a>
+            <Link to="/list/admin/clientes"><i class="fas fa-list"></i> Listar Clientes</Link>
           </li>
           <li>
-            <a href="/budget/admin"><i class="fas fa-file-invoice-dollar"></i> Orçamentos</a>
+            <Link to="/budget/admin"><i class="fas fa-file-invoice-dollar"></i> Orçamentos</Link>
           </li>
           <li>
-            <a href="/metrics/admin"><i class="fas fa-chart-line"></i> Métricas de vendas</a>
+            <Link to="/metrics/admin"><i class="fas fa-chart-line"></i> Métricas de vendas</Link>
           </li>
         </ul>
         <div class="logout-button">
-          <a href="/" class="btn btn-primary">Terminar Sessão</a>
+          <Link to="/" class="btn btn-primary">Terminar Sessão</Link>
         </div>
       </div>
 
