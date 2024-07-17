6b9e4947d77be94cf219cf1cc18ea4117b85884a
joaopombos <pv20407@alunos.estgv.ipv.pt>
2024-07-17 16:34:47 +0100

    admin again

--- src/views/EditAdmin.js
+++ src/views/EditAdmin.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import '../CSS/ware.css';
-import { useNavigate, Link } from 'react-router-dom';
 
 const SoftwareUpdate = () => {
     const { idproduto } = useParams();
